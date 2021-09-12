60fa7decab85573c803e5e28bc24cda23f0e2811
Thunderbird4G <90564792+Thunderbird4G@users.noreply.github.com>
2021-09-12 17:59:05 +0300

    Add files via upload

---
+++ Task7/Task7/Task7_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+void main()
+{
+    cout.precision(4); // формат вывода
+    cout << "vvedite x \n";
+    cin >> x;
+    F();
+    cout << result;
+}
---
+++ Task7/Task7/task7_func.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+double x, result;
+
+void F(void)
+{
+    result = 1 / (sqrt(x) + sqrt(2));
+}
